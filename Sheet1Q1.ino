b17a10752451d7afd64926098b21db2ac8367740
Aly Helmy Hussein <31y7e1my97@gmail.com>
2018-02-19 20:53:03 +0200

    Update Sheet1Q1.ino

--- Sheet1Q1.ino
+++ Sheet1Q1.ino
@@ -5,7 +5,7 @@ void setup() {
 // the loop function runs over and over again forever
 void loop() {
   digitalWrite(13, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(250);              // wait for a second
+  delay(500);            
   digitalWrite(13, LOW);    // turn the LED off by making the voltage LOW
-  delay(1750);              // wait for a second
+  delay(1500);          
 }
